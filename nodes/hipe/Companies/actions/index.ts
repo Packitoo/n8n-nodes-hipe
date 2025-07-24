d605bc2ea4290283d6f33cb6e1d17f7fe1e3e918
Arnaud (Martient) Leherpeur <arnaud@packitoo.com>
2025-07-24 14:51:06 +0200

    feat(company): link & unlink contact to a company

--- nodes/hipe/Companies/actions/index.ts
+++ nodes/hipe/Companies/actions/index.ts
@@ -2,6 +2,8 @@ import * as create from './Create';
 import * as get from './Get';
 import * as list from './List';
 import * as update from './Update';
+import * as linkContact from './linkContact';
+import * as unlinkContact from './unlinkContact';
 
 export const RESOURCE = 'company';
 
@@ -10,6 +12,8 @@ export const ACTIONS = {
   get: get,
   getMany: list,
   update: update,
+  linkContact: linkContact,
+  unlinkContact: unlinkContact,
 };
 
 // Factory function to build all user properties for node usage
@@ -27,6 +31,8 @@ export function buildProperties() {
         { name: 'Get', value: 'get', description: 'Get a specific company', action: 'Get' },
         { name: 'Get Many', value: 'getMany', description: 'Get multiple companies', action: 'Get many' },
         { name: 'Update', value: 'update', description: 'Update a specific company', action: 'Update' },
+        { name: 'Link Contact', value: 'linkContact', description: 'Link an existing contact to a company', action: 'Link contact' },
+        { name: 'Unlink Contact', value: 'unlinkContact', description: 'Unlink a contact from a company', action: 'Unlink contact' },
       ],
       default: 'get',
       noDataExpression: true,
---
+++ nodes/hipe/Companies/actions/unlinkContact.ts
@@ -0,0 +1,66 @@
+import { IExecuteFunctions, INodeExecutionData, INodeProperties } from "n8n-workflow";
+
+export const properties: INodeProperties[] = [
+    {
+        displayName: 'Company ID',
+        name: 'companyId',
+        type: 'string',
+        required: true,
+        default: '',
+        description: 'ID of the company to add the contact to',
+        displayOptions: {
+            show: {
+                resource: ['company'],
+                operation: ['addExistingContact'],
+            },
+        },
+    },
+    {
+        displayName: 'Contact ID',
+        name: 'contactId',
+        type: 'string',
+        required: true,
+        default: '',
+        description: 'ID of the contact to add',
+        displayOptions: {
+            show: {
+                resource: ['company'],
+                operation: ['addExistingContact'],
+            },
+        },
+    }
+]
+
+export async function execute(this: IExecuteFunctions, items: INodeExecutionData[]): Promise<INodeExecutionData[]> {
+    const returnData: INodeExecutionData[] = [];
+    
+	// Get credentials
+	const credentials = await this.getCredentials('hipeApi');
+	let baseUrl = credentials.url;
+	if (typeof baseUrl !== 'string') {
+		throw new Error('HIPE base URL is not a string');
+	}
+	baseUrl = baseUrl.replace(/\/$/, '');
+
+    // Process each item
+    for (let i = 0; i < items.length; i++) {
+    try {
+        const contactId = this.getNodeParameter('contactId', i) as string;
+        const companyId = this.getNodeParameter('companyId', i) as string;
+
+        const response = await this.helpers.requestWithAuthentication.call(this, "hipeApi", {
+            method: 'DELETE',
+            url: `${baseUrl}/api/companies/${companyId}/contacts/${contactId}`,
+            json: true
+        });
+        returnData.push({ json: response });
+    } catch (error) {
+        if (this.continueOnFail()) {
+            returnData.push({ json: { error: error.message } });
+            continue;
+        }
+        throw error;
+    }
+    }
+    return returnData;
+}
